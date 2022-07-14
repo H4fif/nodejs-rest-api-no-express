ec39f60ffc6958d2f9c889f1bacf9026ca6a90ac
h4fif <hafifimammuddyn@gmail.com>
2022-07-14 07:33:36 +0700

    ✨ main file to serve the API and register routes for get all products and get product by id

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const {
+  getProducts,
+  getProduct
+} = require('./controllers/productController');
+
+const server = http.createServer((request, response) => {
+  const { method, url } = request || {};
+
+  if (url === '/api/products' && method === 'GET') {
+    getProducts(request, response);
+  } else if (url.match(/\/api\/products\/[\w]+/) && method === 'GET') {
+    const id = url.split('/')[3];
+    getProduct(request, response, id);
+  } else {
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ message: 'Route not found' }));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
