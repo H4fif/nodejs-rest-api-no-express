e5a64d5a96932627427b7d0bfe24fcc5dffef820
h4fif <hafifimammuddyn@gmail.com>
2022-07-14 09:11:55 +0700

    ✨ add function to create product and save it to file

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,6 @@
 const products = require('../data/products.json');
+const { v4: uuidv4 } = require('uuid');
+const { writeDataToFile } = require('../utils');
 
 /**
  * @function findAll
@@ -14,6 +16,7 @@ const findAll = () => {
 /**
  * @function findById
  * @description query to get product by ID
+ * @param {number} id - the product ID
  * @returns {Promise} Promise
  */
 const findById = (id) => {
@@ -23,7 +26,23 @@ const findById = (id) => {
   });
 };
 
+/**
+ * @function findById
+ * @description query to get product by ID
+ * @param {object} product - new product object refer to product model
+ * @returns {Promise} Promise
+ */
+const create = (product) => {
+  return new Promise((resolve, reject) => {
+    const newProduct = { id: uuidv4(), ...product };
+    products.push(newProduct);
+    writeDataToFile('data/products.json', products);
+    resolve(newProduct);
+  });
+};
+
 module.exports = {
   findAll,
   findById,
+  create,
 };
