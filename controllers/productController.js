cb170a506b080fb2a6a2381ae613ea7006d161d5
h4fif <hafifimammuddyn@gmail.com>
2022-07-14 09:41:53 +0700

    ✨ add function to handle request for update product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,7 @@ const { getRequestBody } = require('../utils');
  * @description Get all products from using findAll() from Product Model
  * @param {http.ClientRequest} req - HTTP Request
  * @param {http.ServerResponse} res - HTTP Response
- * @returns HttpResponse with products in JSON format
+ * @returns {http.ServerResponse} http response with products in JSON format
  */
 const getProducts = async (req, res) => {
   try {
@@ -25,7 +25,7 @@ const getProducts = async (req, res) => {
  * @param {http.ClientRequest} req - HTTP Request
  * @param {http.ServerResponse} res - HTTP Response
  * @param {integer} id - the product ID
- * @returns HttpResponse with product if exists or a message if not found in JSON format
+ * @returns {http.ServerResponse} http response with product if exists or a message if not found in JSON format
  */
 const getProduct = async (req, res, id) => {
   try {
@@ -48,7 +48,7 @@ const getProduct = async (req, res, id) => {
  * @description Create product using create() from Product Model
  * @param {http.ClientRequest} req - HTTP Request
  * @param {http.ServerResponse} res - HTTP Response
- * @returns HttpResponse
+ * @returns {http.ServerResponse} http response
  */
 const createProduct = async (req, res) => {
   try {
@@ -64,8 +64,44 @@ const createProduct = async (req, res) => {
   }
 };
 
+/**
+ * @function updateProduct
+ * @description Create product using create() from Product Model
+ * @param {http.ClientRequest} req - HTTP Request
+ * @param {http.ServerResponse} res - HTTP Response
+ * @param {number} id - the product ID
+ * @returns {http.ServerResponse} http response
+ */
+const updateProduct = async (req, res, id) => {
+  try {
+    const findProduct = await Product.findById(id);
+
+    if (!findProduct) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify({ message: 'Product not found' }));
+    } else {
+      const body = await getRequestBody(req);
+      const { title, description, price } = JSON.parse(body);
+
+      const product = {
+        title: title || findProduct.title,
+        description: description || findProduct.description,
+        price: price || findProduct.price,
+      };
+
+      const updatedProduct = await Product.update(id, product);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      return res.end(JSON.stringify(updatedProduct));
+    }
+  } catch (error) {
+    console.log('ERROR GET PRODUCTS >>> ', error);
+  }
+};
+
 module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct,
 };
