7fed539072c3cad96a1260d4b381d64de62a3240
h4fif <hafifimammuddyn@gmail.com>
2022-07-14 09:23:16 +0700

    ✨ add function to parse the http client request body

--- utils.js
+++ utils.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * @function writeDataToFile
+ * @param {string} fileName - file name with extension and its relative directory path
+ * @param {array|object} content - the content to write into the file
+ */
 const writeDataToFile = (fileName, content) => {
   const file = path.resolve() + '/' + fileName;
   fs.writeFileSync(file, JSON.stringify(content), 'utf-8', (error) => {
@@ -8,6 +13,25 @@ const writeDataToFile = (fileName, content) => {
   });
 };
 
+/**
+ * @function getRequestBody
+ * @param {http.ClientRequest} req
+ * @returns {promise} Promise
+ */
+const getRequestBody = (req) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let body = '';
+      req.on('data', (chunk) => body += chunk.toString());
+      req.on('end', () => resolve(body));
+    } catch (error) {
+      console.log('ERROR ON GET REQUEST BODY >>>> ', error);
+      reject(error);
+    }
+  });
+};
+
 module.exports = {
   writeDataToFile,
+  getRequestBody,
 };
